bbb403dcff57ec147b44aca537e344c96933a646
Siwa <64341316+Clouza@users.noreply.github.com>
2021-04-20 21:20:05 +0800

    add client info

--- index.php
+++ index.php
@@ -1,7 +1,143 @@
 <?php
-$localhost = 'C:\xampp\htdocs'; // directory address
+$localhost = $_SERVER['DOCUMENT_ROOT']; // directory address
 
 $dir = array_diff(scandir($localhost), array('..', '.')); // remove dots
+
+// user info
+class UserInfo
+{
+    private static function get_user_agent()
+    {
+        return  $_SERVER['HTTP_USER_AGENT'];
+    }
+
+    public static function get_os()
+    {
+
+        $user_agent = self::get_user_agent();
+        $os_platform    =   "Unknown OS Platform";
+        $os_array       =   array(
+            '/windows nt 10/i'         =>  'Windows 10',
+            '/windows nt 6.3/i'     =>  'Windows 8.1',
+            '/windows nt 6.2/i'     =>  'Windows 8',
+            '/windows nt 6.1/i'     =>  'Windows 7',
+            '/windows nt 6.0/i'     =>  'Windows Vista',
+            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+            '/windows nt 5.1/i'     =>  'Windows XP',
+            '/windows xp/i'         =>  'Windows XP',
+            '/windows nt 5.0/i'     =>  'Windows 2000',
+            '/windows me/i'         =>  'Windows ME',
+            '/win98/i'              =>  'Windows 98',
+            '/win95/i'              =>  'Windows 95',
+            '/win16/i'              =>  'Windows 3.11',
+            '/macintosh|mac os x/i' =>  'Mac OS X',
+            '/mac_powerpc/i'        =>  'Mac OS 9',
+            '/linux/i'              =>  'Linux',
+            '/ubuntu/i'             =>  'Ubuntu',
+            '/iphone/i'             =>  'iPhone',
+            '/ipod/i'               =>  'iPod',
+            '/ipad/i'               =>  'iPad',
+            '/android/i'            =>  'Android',
+            '/blackberry/i'         =>  'BlackBerry',
+            '/webos/i'              =>  'Mobile'
+        );
+
+        foreach ($os_array as $regex => $value) {
+            if (preg_match($regex, $user_agent)) {
+                $os_platform    =   $value;
+            }
+        }
+        return $os_platform;
+    }
+
+    public static function  get_browser()
+    {
+
+        $user_agent = self::get_user_agent();
+
+        $browser        =   "Unknown Browser";
+
+        $browser_array  =   array(
+            '/msie/i'       =>  'Internet Explorer',
+            '/Trident/i'    =>  'Internet Explorer',
+            '/firefox/i'    =>  'Firefox',
+            '/safari/i'     =>  'Safari',
+            '/chrome/i'     =>  'Chrome',
+            '/edge/i'       =>  'Edge',
+            '/opera/i'      =>  'Opera',
+            '/netscape/i'   =>  'Netscape',
+            '/maxthon/i'    =>  'Maxthon',
+            '/konqueror/i'  =>  'Konqueror',
+            '/ubrowser/i'   =>  'UC Browser',
+            '/mobile/i'     =>  'Handheld Browser'
+        );
+
+        foreach ($browser_array as $regex => $value) {
+
+            if (preg_match($regex, $user_agent)) {
+                $browser    =   $value;
+            }
+        }
+
+        return $browser;
+    }
+
+    public static function  get_device()
+    {
+
+        $tablet_browser = 0;
+        $mobile_browser = 0;
+
+        if (preg_match('/(tablet|ipad|playbook)|(android(?!.*(mobi|opera mini)))/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+            $tablet_browser++;
+        }
+
+        if (preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|android|iemobile)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+            $mobile_browser++;
+        }
+
+        if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']), 'application/vnd.wap.xhtml+xml') > 0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
+            $mobile_browser++;
+        }
+
+        $mobile_ua = strtolower(substr(self::get_user_agent(), 0, 4));
+        $mobile_agents = array(
+            'w3c ', 'acs-', 'alav', 'alca', 'amoi', 'audi', 'avan', 'benq', 'bird', 'blac',
+            'blaz', 'brew', 'cell', 'cldc', 'cmd-', 'dang', 'doco', 'eric', 'hipt', 'inno',
+            'ipaq', 'java', 'jigs', 'kddi', 'keji', 'leno', 'lg-c', 'lg-d', 'lg-g', 'lge-',
+            'maui', 'maxo', 'midp', 'mits', 'mmef', 'mobi', 'mot-', 'moto', 'mwbp', 'nec-',
+            'newt', 'noki', 'palm', 'pana', 'pant', 'phil', 'play', 'port', 'prox',
+            'qwap', 'sage', 'sams', 'sany', 'sch-', 'sec-', 'send', 'seri', 'sgh-', 'shar',
+            'sie-', 'siem', 'smal', 'smar', 'sony', 'sph-', 'symb', 't-mo', 'teli', 'tim-',
+            'tosh', 'tsm-', 'upg1', 'upsi', 'vk-v', 'voda', 'wap-', 'wapa', 'wapi', 'wapp',
+            'wapr', 'webc', 'winw', 'winw', 'xda ', 'xda-'
+        );
+
+        if (in_array($mobile_ua, $mobile_agents)) {
+            $mobile_browser++;
+        }
+
+        if (strpos(strtolower(self::get_user_agent()), 'opera mini') > 0) {
+            $mobile_browser++;
+            //Check for tablets on opera mini alternative headers
+            $stock_ua = strtolower(isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA']) ? $_SERVER['HTTP_X_OPERAMINI_PHONE_UA'] : (isset($_SERVER['HTTP_DEVICE_STOCK_UA']) ? $_SERVER['HTTP_DEVICE_STOCK_UA'] : ''));
+            if (preg_match('/(tablet|ipad|playbook)|(android(?!.*mobile))/i', $stock_ua)) {
+                $tablet_browser++;
+            }
+        }
+
+        if ($tablet_browser > 0) {
+            // do something for tablet devices
+            return 'Tablet';
+        } else if ($mobile_browser > 0) {
+            // do something for mobile devices
+            return 'Mobile';
+        } else {
+            // do something for everything else
+            return 'Computer';
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +147,7 @@ $dir = array_diff(scandir($localhost), array('..', '.')); // remove dots
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>localhost</title>
+    <title><?= $_SERVER['SERVER_NAME']; ?></title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -20,7 +156,15 @@ $dir = array_diff(scandir($localhost), array('..', '.')); // remove dots
 <body>
 
     <div class="container">
-        <h1>Welcome to localhost</h1>
+        <h1>Welcome to <?= $_SERVER['SERVER_NAME']; ?></h1>
+        <small><?php
+                echo $_SERVER['REMOTE_ADDR'];
+                echo ' |Doc Root: ' . $_SERVER['DOCUMENT_ROOT'];
+                echo ' |Browser: ' . UserInfo::get_browser();
+                echo ' |Device: ' . UserInfo::get_device();
+                echo ' |System: ' . UserInfo::get_os();
+                ?>
+        </small>
 
         <div class="card text-center">
             <div class="card-header" data-bs-toggle="collapse" data-bs-target="#directoryLocalhost" title="Klik untuk melihat">
@@ -39,6 +183,7 @@ $dir = array_diff(scandir($localhost), array('..', '.')); // remove dots
         </div>
 
         <hr>
+
         <h1>Workspace</h1>
         <small>Use the workspace to test code without creating folders</small>
     </div>
